d13b7a381b5fe920642ee21297c3083196d49557
Cinnamon-Akuoma <cboyle.george@gmail.com>
2023-01-03 14:17:39 -0800

    task done

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,8 @@
+i#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+
+class LockedClass:
+    """A locked class that only lets the user dynamically create the instance
+    attribute 'first_name'"""
+    __slots__ = ['first_name']
