e67292ca3e19ced83d68e564a28823f3a8e8c888
Cinnamon-Akuoma <cboyle.george@gmail.com>
2023-01-11 09:59:01 -0800

    task accomplished

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tru jan 3 15:13:37 2023
+@author: Nathn Orobor
+"""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """
+    Save object to a file
+    Arguments:
+        my_obj (obj): The inputed object to convert in json format
+        filename (str): The name of the output file
+    Return:
+        A file with a text in jason format
+    """
+    with open(filename, 'w', encoding='utf-8') as file:
+        return file.write(json.dumps(my_obj))
